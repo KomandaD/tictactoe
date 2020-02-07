ac65857fc722628998e99510973b16aff2cb60f7
KomandaD <54846912+KomandaD@users.noreply.github.com>
2020-02-07 16:22:50 +0300

    Add files via upload

---
+++ precache-manifest.da8dec52f1db2c9634b45f4f6741c3bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22de8094fdd359b3a20f8e41fcc72da3",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "9735e5bfc0ffb7e61c6a",
+    "url": "/tictactoe/static/css/main.8d329e1f.chunk.css"
+  },
+  {
+    "revision": "9644ab735dd0446ce9a8",
+    "url": "/tictactoe/static/js/2.36b1c09a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/tictactoe/static/js/2.36b1c09a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9735e5bfc0ffb7e61c6a",
+    "url": "/tictactoe/static/js/main.0f6283e7.chunk.js"
+  },
+  {
+    "revision": "f5ae1eb9ecb968a2bdfc",
+    "url": "/tictactoe/static/js/runtime-main.9ff60698.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tictactoe/static/media/logo.5d5d9eef.svg"
+  }
+]);
